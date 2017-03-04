d217896218a25813cc1fcbf15be38eb4c573d37d
AzureInterface <azureinterface@protonmail.com>
2017-03-04 17:02:04 +0000

    added lab 2 to mod 1

---
+++ Module1/LAB2/Push-DscConfigToVm.ps1
@@ -0,0 +1,41 @@
+﻿[CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true)]
+        $StorageAccountName,
+
+        [Parameter(Mandatory=$true)]
+        $ResourceGroupName,
+
+        [Parameter(Mandatory=$true)]
+        $ConfigPath,
+
+        [Parameter(Mandatory=$true)]
+        $ConfigName,
+
+        [Parameter(Mandatory=$true)]
+        $VMName
+    )
+
+#Push the config to Azure Storage
+
+$pubParams = @{
+    ConfigurationPath = $ConfigPath
+    ResourceGroupName = $ResourceGroupName
+    StorageAccountName = $StorageAccountName
+}
+
+Publish-AzureRmVMDscConfiguration @pubParams
+
+#Apply the configuration on the VM
+
+$setParams = @{
+    ResourceGroupName = $ResourceGroupName
+    ArchiveStorageAccountName = $StorageAccountName
+    ArchiveBlobName = "$($ConfigPath.split('\')[-1]).zip"
+    VMName = $VMName
+    Version = '2.15'
+    ConfigurationName = $ConfigName
+    WmfVersion = '4.0'
+}
+
+Set-AzureRmVMDscExtension @setParams
